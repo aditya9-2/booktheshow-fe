7870177712fa117517d882e949427b2fa1d367ca
Aditya Basak <adityabasak7031@gmail.com>
2026-09-21 20:02:26 +0530

    new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom"
+import { useEffect } from "react"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
@@ -8,37 +9,42 @@ import CategoryGrid from "./components/CategoryGrid"
 import PageLoader from "./components/PageLoader"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
-import { useAuthStore } from "./store/authStore"
-import { useEffect } from "react"
 import About from "./pages/About"
-
+import NotFound from "./pages/NotFound"
+import { useAuthStore } from "./store/authStore"
+import Events from "./pages/Events"
+import EventDetail from "./pages/EventDetail"
 
 const AUTH_ROUTES = ["/signin", "/signup"]
 
-
 export const App = () => {
-
   const { pathname } = useLocation()
   const isAuthRoute = AUTH_ROUTES.includes(pathname)
+  const isLandingPage = pathname === "/"
   const hydrate = useAuthStore((s) => s.hydrate)
 
-    useEffect(() => {
-        hydrate()
-    }, [hydrate])
+  useEffect(() => {
+    hydrate()
+  }, [hydrate])
 
-
-  return (
-    <PageLoader>
+  const content = (
+    <>
       {!isAuthRoute && <Navbar />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
       {!isAuthRoute && <Footer />}
-    </PageLoader>
+    </>
   )
+
+  return isLandingPage ? <PageLoader>{content}</PageLoader> : content
 }
 
 const LandingPage = () => {
@@ -51,4 +57,3 @@ const LandingPage = () => {
     </>
   )
 }
-
