00d3a04b51a9ee91b6bd0a372e4e904494ae46f1
Aditya Basak <adityabasak7031@gmail.com>
2026-09-21 19:45:19 +0530

    add booking endpoints

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { EventItem } from "./types";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -7,18 +8,33 @@ export const api = axios.create({
     },
 })
 
-export type SignupPayload = { 
-    name: string; 
-    email: string; 
-    password: string 
-}
-export type SigninPayload = { 
-    email: string; 
-    password: string 
-}
-export type SigninResponse = { 
-    token: string 
+export type SignupPayload = {
+    name: string;
+    email: string;
+    password: string
 }
 
-export const signup = (payload: SignupPayload) => api.post("/auth/signup", payload)
-export const signin = (payload: SigninPayload) => api.post<SigninResponse>("/auth/signin", payload)
+export type SigninPayload = {
+    email: string;
+    password: string
+}
+
+export type SigninResponse = {
+    token: string
+}
+
+export type AllEventsResponse = {
+    count: number;
+    events: EventItem[];
+    message?: string
+}
+
+export type SingleEventResponse = {
+    event: EventItem
+}
+
+export const signup = (payload: SignupPayload) => api.post("/auth/signup", payload);
+export const signin = (payload: SigninPayload) => api.post<SigninResponse>("/auth/signin", payload);
+
+export const getAllEvents = () => api.get<AllEventsResponse>("/event/all");
+export const getEventById = (id: string) => api.get<SingleEventResponse>(`event/${id}`);
