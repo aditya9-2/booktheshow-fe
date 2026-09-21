dadfee71f5bae209aafa2e3f8a942c6a5be82704
Aditya Basak <adityabasak7031@gmail.com>
2026-09-22 00:19:31 +0530

    add booking types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -13,3 +13,48 @@ export type EventItem = {
     posterUrl?: string
     sections: EventSection[]
 }
+
+// the fuckin culprit - "erasableSyntaxOnly": true, cannot use enums!
+export const BookingStatus = {
+    Booked: "booked",
+    Cancelled: "cancelled",
+} as const;
+
+export type BookingStatus = (typeof BookingStatus)[keyof typeof BookingStatus]
+
+export type Booking = {
+    _id: string
+    userId: string
+    eventId: string
+    sectionId: string
+    quantity: number
+    priceAtBooking: number
+    status: BookingStatus
+    idempotencyKey: string
+    createdAt: string
+}
+
+export type MyBooking = {
+    _id: string
+    eventName: string
+    eventDate: string
+    sectionName: string
+    quantity: number
+    priceAtBooking: number
+    createdAt: string
+}
+
+export type CreateBookingPayload = {
+    eventId: string
+    sectionId: string
+    quantity: number
+    idempotencyKey: string
+}
+
+export type CreateBookingResponse = {
+    message: string; booking: Booking
+}
+
+export type MyBookingsResponse = {
+    bookings: MyBooking[]
+}
