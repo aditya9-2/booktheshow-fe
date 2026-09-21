ef7690a684dcd02ee4469b4c48ae48b56b1a3d2d
Aditya Basak <adityabasak7031@gmail.com>
2026-09-21 14:41:27 +0530

    restructured app with a landing page function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Routes, Route } from "react-router-dom"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import FeaturedEvents from "./components/FeaturedEvents"
+import Concierge from "./components/Concierge"
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
+      <Footer />
+    </>
+  )
+}
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedEvents />
+      <Concierge />
     </>
   )
 }
